4eb67c8cb93b8d73d426e36d4a6c768bc6b81888
mxro <mxro@nowhere.com>
2015-07-09 19:16:21 +1000

    Reworking API Autocommit Thu Jul 09 19:16:21 EST 2015

--- src/test/java/de/mxro/async/map/tests/TestThatParellelWorkerProcessesPuts.java
+++ src/test/java/de/mxro/async/map/tests/TestThatParellelWorkerProcessesPuts.java
@@ -1,53 +1,29 @@
 package de.mxro.async.map.tests;
 
-import de.mxro.async.map.AsyncMap;
-import de.mxro.async.map.AsyncMaps;
-import de.mxro.async.map.jre.AsyncMapsJre;
-import delight.async.AsyncCommon;
-import delight.async.Operation;
-import delight.async.callbacks.SimpleCallback;
-import delight.async.callbacks.ValueCallback;
-import delight.async.jre.Async;
-import delight.functional.Success;
-import org.junit.Test;
-
 @SuppressWarnings("all")
 public class TestThatParellelWorkerProcessesPuts {
-  @Test
-  public void test() {
-    AsyncMap<String, String> _hashMap = AsyncMaps.<String, String>hashMap();
-    final AsyncMap<String, String> map = AsyncMapsJre.<String, String>divideWork(4, _hashMap);
-    final Operation<Success> _function = new Operation<Success>() {
-      @Override
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = AsyncCommon.wrap(callback);
-        map.start(_wrap);
-      }
-    };
-    Async.<Success>waitFor(_function);
-    final Operation<Success> _function_1 = new Operation<Success>() {
-      @Override
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = AsyncCommon.wrap(callback);
-        map.put("1", "one", _wrap);
-      }
-    };
-    Async.<Success>waitFor(_function_1);
-    final Operation<Success> _function_2 = new Operation<Success>() {
-      @Override
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = AsyncCommon.wrap(callback);
-        map.put("1", "one", _wrap);
-      }
-    };
-    Async.<Success>waitFor(_function_2);
-    final Operation<Success> _function_3 = new Operation<Success>() {
-      @Override
-      public void apply(final ValueCallback<Success> callback) {
-        SimpleCallback _wrap = AsyncCommon.wrap(callback);
-        map.stop(_wrap);
-      }
-    };
-    Async.<Success>waitFor(_function_3);
+  /* @Test
+   */public void test() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field Async is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method start is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field AsyncCommon is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field Async is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method put is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field AsyncCommon is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field Async is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method put is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field AsyncCommon is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field Async is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method stop is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nThe method or field AsyncCommon is undefined for the type TestThatParellelWorkerProcessesPuts"
+      + "\nwaitFor cannot be resolved"
+      + "\nwrap cannot be resolved"
+      + "\nwaitFor cannot be resolved"
+      + "\nwrap cannot be resolved"
+      + "\nwaitFor cannot be resolved"
+      + "\nwrap cannot be resolved"
+      + "\nwaitFor cannot be resolved"
+      + "\nwrap cannot be resolved");
   }
 }
