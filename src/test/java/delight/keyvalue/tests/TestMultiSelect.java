e1d62dc1684e4b76d9be066de2a774d6ea14fe9d
mxro <mxro@nowhere.com>
2015-07-11 14:38:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 14:38:16 EST 2015

--- src/test/java/delight/keyvalue/tests/TestMultiSelect.java
+++ src/test/java/delight/keyvalue/tests/TestMultiSelect.java
@@ -1,10 +1,51 @@
 package delight.keyvalue.tests;
 
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.async.jre.Async;
+import delight.functional.Success;
+import delight.keyvalue.Store;
+import delight.keyvalue.Stores;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestMultiSelect {
   @Test
   public void test() {
+    final Store<String, String> store = Stores.<String, String>hashMap();
+    final Operation<Success> _function = new Operation<Success>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        store.start(_asSimpleCallback);
+      }
+    };
+    Async.<Success>waitFor(_function);
+    final Operation<Success> _function_1 = new Operation<Success>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        store.put("node/child1", "one", _asSimpleCallback);
+      }
+    };
+    Async.<Success>waitFor(_function_1);
+    final Operation<Success> _function_2 = new Operation<Success>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        store.put("node/child2", "two", _asSimpleCallback);
+      }
+    };
+    Async.<Success>waitFor(_function_2);
+    final Operation<Success> _function_3 = new Operation<Success>() {
+      @Override
+      public void apply(final ValueCallback<Success> callback) {
+        SimpleCallback _asSimpleCallback = AsyncCommon.asSimpleCallback(callback);
+        store.stop(_asSimpleCallback);
+      }
+    };
+    Async.<Success>waitFor(_function_3);
   }
 }
--- src/test/xtend/delight/keyvalue/tests/TestMultiSelect.xtend
+++ src/test/xtend/delight/keyvalue/tests/TestMultiSelect.xtend
@@ -1,11 +1,33 @@
 package delight.keyvalue.tests
 
+import delight.async.AsyncCommon
+import delight.async.jre.Async
+import delight.keyvalue.Stores
 import org.junit.Test
 
 class TestMultiSelect {
 	
 	@Test
 	def void test() {
+		
+		val store = Stores.hashMap
+		
+		Async.waitFor [ callback |
+			store.start(AsyncCommon.asSimpleCallback(callback));
+		]
+	
+		Async.waitFor [ callback |
+			store.put("node/child1", "one", AsyncCommon.asSimpleCallback(callback));
+		]
+
+		Async.waitFor [ callback |
+			store.put("node/child2", "two", AsyncCommon.asSimpleCallback(callback));
+		]
+		
+	
+		Async.waitFor [ callback |
+			store.stop(AsyncCommon.asSimpleCallback(callback));
+		]
 		
 	}
 	
