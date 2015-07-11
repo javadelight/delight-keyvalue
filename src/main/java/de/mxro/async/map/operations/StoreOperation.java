d0dc29f67433d4b6fb8d74d234b43c03b157bf2c
mxro <mxro@nowhere.com>
2015-07-11 10:33:09 +1000

    Implementing key value database Autocommit Sat Jul 11 10:33:09 EST 2015

--- src/main/java/de/mxro/async/map/operations/StoreOperation.java
+++ src/main/java/de/mxro/async/map/operations/StoreOperation.java
@@ -1,5 +1,7 @@
 package de.mxro.async.map.operations;
 
+import delight.async.callbacks.ValueCallback;
+
 import de.mxro.async.map.Store;
 
 /**
@@ -8,8 +10,8 @@ import de.mxro.async.map.Store;
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
  */
-public interface StoreOperation<K, V> {
+public interface StoreOperation<K, V, R> {
 
-    public void applyOn(Store<K, V> store);
+    public void applyOn(Store<K, V> store, ValueCallback<R> callback);
 
 }
