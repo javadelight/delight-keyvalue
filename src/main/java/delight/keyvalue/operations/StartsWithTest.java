dd9ed8918c247514554c8130d5c5529e842374b8
mxro <mxro@nowhere.com>
2015-07-11 12:18:03 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 12:18:03 EST 2015

--- src/main/java/delight/keyvalue/operations/StartsWithTest.java
+++ src/main/java/delight/keyvalue/operations/StartsWithTest.java
@@ -1,5 +1,24 @@
 package delight.keyvalue.operations;
 
-public class StartsWithTest {
+import delight.functional.Function;
+
+public class StartsWithTest implements Function<String, Boolean> {
+
+    private final String startsWith;
+
+    public final String startsWith() {
+        return startsWith;
+    }
+
+    @Override
+    public Boolean apply(final String input) {
+
+        return input.startsWith(startsWith);
+    }
+
+    public StartsWithTest(final String startsWith) {
+        super();
+        this.startsWith = startsWith;
+    }
 
 }
