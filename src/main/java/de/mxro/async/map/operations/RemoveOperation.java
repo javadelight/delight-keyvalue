746c21e6adcd65288a46a8c0ea56d2d8d17d7cbc
mxro <mxro@nowhere.com>
2015-07-11 10:39:09 +1000

    Implementing key value database Autocommit Sat Jul 11 10:39:09 EST 2015

--- src/main/java/de/mxro/async/map/operations/RemoveOperation.java
+++ src/main/java/de/mxro/async/map/operations/RemoveOperation.java
@@ -1,8 +1,10 @@
 package de.mxro.async.map.operations;
 
-import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.functional.Success;
 
 import de.mxro.async.map.Store;
+import de.mxro.async.map.StoreImplementation;
 
 /**
  * An object representation of a remove operation on an {@link Store}.
@@ -10,23 +12,23 @@ import de.mxro.async.map.Store;
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
  */
-public class RemoveOperation<K> implements StoreOperation {
+public class RemoveOperation<K, V> implements StoreOperation<K, V, Success> {
 
-	private final K key;
-	private final SimpleCallback callback;
+    private final K key;
 
-	public K getKey() {
-		return key;
-	}
+    public K getKey() {
+        return key;
+    }
 
-	public SimpleCallback getCallback() {
-		return callback;
-	}
+    public RemoveOperation(final K key) {
+        super();
+        this.key = key;
 
-	public RemoveOperation(K key, SimpleCallback callback) {
-		super();
-		this.key = key;
-		this.callback = callback;
-	}
+    }
+
+    @Override
+    public void applyOn(final StoreImplementation<K, V> store, final ValueCallback<Success> callback) {
+        store.remove(key, callback);
+    }
 
 }
