f3d2ad3ebc1fee998749aa87192da7c7de508881
mxro <mxro@nowhere.com>
2015-07-11 11:32:03 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 11:32:03 EST 2015

--- src/main/java/delight/keyvalue/Store.java
+++ src/main/java/delight/keyvalue/Store.java
@@ -30,6 +30,8 @@ public interface Store<K, V> extends Service {
 
     public void removeSync(K key);
 
+    public void removeAll(K commonKeyElement, SimpleCallback callback);
+
     /**
      * Assures that a connection to the persistence medium is established and
      * the map is operational.
