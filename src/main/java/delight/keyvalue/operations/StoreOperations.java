8e34a4ff4674102d05f2ee1f8c086096ad3fbc01
mxro <mxro@nowhere.com>
2015-07-11 14:46:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 14:46:15 EST 2015

--- src/main/java/delight/keyvalue/operations/StoreOperations.java
+++ src/main/java/delight/keyvalue/operations/StoreOperations.java
@@ -1,6 +1,9 @@
 package delight.keyvalue.operations;
 
+import delight.functional.Closure;
+import delight.keyvalue.StoreEntry;
 import delight.keyvalue.internal.operations.ClearCacheOperation;
+import delight.keyvalue.internal.operations.GetAllOperation;
 import delight.keyvalue.internal.operations.RemoveAllOperation;
 
 public class StoreOperations {
@@ -11,6 +14,11 @@ public class StoreOperations {
 
     public static <V> StoreOperation<String, V> removeAll(final String keyStartsWith) {
         return new RemoveAllOperation<V>(keyStartsWith);
+    }
+
+    public static <V> StoreOperation<String, V> getAll(final String keyStartsWith,
+            final Closure<StoreEntry<String, V>> onEntry) {
+        return new GetAllOperation<V>(keyStartsWith, onEntry);
     }
 
 }
