c0c4ad3aef347d1c92614ea649572ecee7dd8a30
mxro <mxro@nowhere.com>
2015-07-11 14:37:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 14:37:16 EST 2015

--- src/test/java/delight/keyvalue/tests/TestMultiSelect.java
+++ src/test/java/delight/keyvalue/tests/TestMultiSelect.java
@@ -1,5 +1,10 @@
 package delight.keyvalue.tests;
 
+import org.junit.Test;
+
 @SuppressWarnings("all")
 public class TestMultiSelect {
+  @Test
+  public void test() {
+  }
 }
--- src/test/xtend/delight/keyvalue/tests/TestMultiSelect.xtend
+++ src/test/xtend/delight/keyvalue/tests/TestMultiSelect.xtend
@@ -1,5 +1,12 @@
 package delight.keyvalue.tests
 
+import org.junit.Test
+
 class TestMultiSelect {
+	
+	@Test
+	def void test() {
+		
+	}
 	
 }
