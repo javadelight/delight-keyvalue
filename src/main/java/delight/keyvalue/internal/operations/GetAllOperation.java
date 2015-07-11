c51436dbe8e1fbe5ede44bd67b235c5dc691fde9
mxro <mxro@nowhere.com>
2015-07-11 14:40:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 14:40:16 EST 2015

--- src/main/java/delight/keyvalue/internal/operations/GetAllOperation.java
+++ src/main/java/delight/keyvalue/internal/operations/GetAllOperation.java
@@ -1,7 +1,45 @@
 package delight.keyvalue.internal.operations;
 
+import delight.async.AsyncCommon;
+import delight.async.callbacks.ValueCallback;
+import delight.functional.Closure;
+import delight.functional.Function;
+import delight.keyvalue.StoreEntry;
+import delight.keyvalue.StoreImplementation;
 import delight.keyvalue.operations.StoreOperation;
 
 public class GetAllOperation<V> implements StoreOperation<String, V> {
+
+    private String keyStartsWith;
+    private Closure<StoreEntry<String, V>> onEntry;
+
+    @Override
+    public void modifyKeys(final Function<String, String> func) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void ignoreKeys(final Function<String, Boolean> test) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void modifyValuesBeforePut(final Function<V, V> func) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void modifyValuesAfterGet(final Function<V, V> func) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void applyOn(final StoreImplementation<String, V> store, final ValueCallback<Object> callback) {
+        store.getAll(keyStartsWith, onEntry, AsyncCommon.asSimpleCallbackAndReturnSuccess(callback));
+    }
 
 }
