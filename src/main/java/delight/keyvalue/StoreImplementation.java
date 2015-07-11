f8faa9c4f643fff84000e48fdd7fd86342e10bbd
mxro <mxro@nowhere.com>
2015-07-11 14:56:16 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 14:56:16 EST 2015

--- src/main/java/delight/keyvalue/StoreImplementation.java
+++ src/main/java/delight/keyvalue/StoreImplementation.java
@@ -1,6 +1,7 @@
 package delight.keyvalue;
 
 import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
 import delight.functional.Closure;
 
 public interface StoreImplementation<K, V> extends Store<K, V> {
@@ -10,5 +11,7 @@ public interface StoreImplementation<K, V> extends Store<K, V> {
     public void removeAll(String keyStartsWith, SimpleCallback callback);
 
     public void getAll(String keyStartsWith, final Closure<StoreEntry<K, V>> onEntry, final SimpleCallback onCompleted);
+
+    public void count(String keyStartsWith, final ValueCallback<Integer> callback);
 
 }
