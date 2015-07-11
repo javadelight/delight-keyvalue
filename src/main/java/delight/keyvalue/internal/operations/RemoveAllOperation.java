195274e8a67f57ba3fd4c64b25050d3f8cabef74
mxro <mxro@nowhere.com>
2015-07-11 12:32:03 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 12:32:03 EST 2015

--- src/main/java/delight/keyvalue/internal/operations/RemoveAllOperation.java
+++ src/main/java/delight/keyvalue/internal/operations/RemoveAllOperation.java
@@ -7,37 +7,21 @@ import delight.functional.Success;
 import delight.keyvalue.StoreImplementation;
 import delight.keyvalue.operations.StoreOperation;
 
-public class RemoveAllOperation<K, V> implements StoreOperation<K, V> {
+public class RemoveAllOperation<V> implements StoreOperation<String, V> {
 
     String keyStartsWith;
 
     boolean skip = false;
 
     @Override
-    public void modifyKeys(final Function<K, K> func) {
-        if (keyTest instanceof StartsWithTest) {
-            final StartsWithTest startsWithTest = (StartsWithTest) keyTest;
-
-            keyTest = (Function<K, Boolean>) new StartsWithTest((String) func.apply((K) startsWithTest.startsWith()));
-            return;
-
-        }
-
-        final Function<K, Boolean> oldKeyTest = keyTest;
-        keyTest = new Function<K, Boolean>() {
-
-            @Override
-            public Boolean apply(final K input) {
-                return oldKeyTest.apply(func.apply(input));
-            }
-
-        };
+    public void modifyKeys(final Function<String, String> func) {
+        keyStartsWith = func.apply(keyStartsWith);
 
     }
 
     @Override
-    public void ignoreKeys(final Function<K, Boolean> test) {
-        if (test.apply(commonKey)) {
+    public void ignoreKeys(final Function<String, Boolean> test) {
+        if (test.apply(keyStartsWith)) {
             skip = true;
         }
     }
