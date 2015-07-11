65e0196f3e1aa6470dd2bae3aee2ed8338984d42
mxro <mxro@nowhere.com>
2015-07-11 11:03:02 +1000

    Reworking generic operations on stores Autocommit Sat Jul 11 11:03:02 EST 2015

--- src/main/java/delight/keyvalue/operations/StoreOperations.java
+++ src/main/java/delight/keyvalue/operations/StoreOperations.java
@@ -4,8 +4,8 @@ import delight.keyvalue.internal.operations.ClearCacheOperation;
 
 public class StoreOperations {
 
-    public static StoreOperation<Object, Object> clearCache() {
-        return new ClearCacheOperation<Object, Object>();
+    public static <K, V> StoreOperation<K, V> clearCache() {
+        return new ClearCacheOperation<K, V>();
     }
 
 }
