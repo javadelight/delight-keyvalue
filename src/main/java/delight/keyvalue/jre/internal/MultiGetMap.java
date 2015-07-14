8a4a270efd39d52dd83ddc4c1507ce3bf388b157
mxro <mxro@nowhere.com>
2015-07-14 10:52:10 +1000

    Implementing new database engine Autocommit Tue Jul 14 10:52:10 EST 2015

--- src/main/java/delight/keyvalue/jre/internal/MultiGetMap.java
+++ src/main/java/delight/keyvalue/jre/internal/MultiGetMap.java
@@ -1,5 +1,14 @@
 package delight.keyvalue.jre.internal;
 
-public class MultiGetMap {
+import delight.keyvalue.Store;
+
+/**
+ * <p>
+ * Bundles gets, even getSync ones.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+public class MultiGetMap<K, V> implements Store<K, V> {
 
 }
