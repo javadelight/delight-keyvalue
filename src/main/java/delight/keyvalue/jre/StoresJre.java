33aba13d37774567d877381e5db7ce7470a63761
mxro <mxro@nowhere.com>
2015-07-14 11:29:10 +1000

    Implementing new database engine Autocommit Tue Jul 14 11:29:10 EST 2015

--- src/main/java/delight/keyvalue/jre/StoresJre.java
+++ src/main/java/delight/keyvalue/jre/StoresJre.java
@@ -2,6 +2,7 @@ package delight.keyvalue.jre;
 
 import delight.keyvalue.Store;
 import delight.keyvalue.Stores;
+import delight.keyvalue.jre.internal.MultiGetMap;
 import delight.keyvalue.jre.internal.SplitWorkerThreadsMapConnection;
 
 import java.util.Collections;
@@ -9,18 +10,16 @@ import java.util.WeakHashMap;
 
 public class StoresJre {
 
-	public static final <K, V> Store<K, V> cacheWithWeakReferences(
-			Store<K, V> decorated) {
-		return Stores.cache(
-				Collections.synchronizedMap(new WeakHashMap<K, Object>()),
-				decorated);
-	}
+    public static final <K, V> Store<K, V> cacheWithWeakReferences(final Store<K, V> decorated) {
+        return Stores.cache(Collections.synchronizedMap(new WeakHashMap<K, Object>()), decorated);
+    }
 
-	public static final <K, V> Store<K, V> divideWork(int workerThreads,
-			Store<K, V> decorated) {
-		return new SplitWorkerThreadsMapConnection<K, V>(decorated,
-				workerThreads);
-	}
-	
+    public static final <K, V> Store<K, V> divideWork(final int workerThreads, final Store<K, V> decorated) {
+        return new SplitWorkerThreadsMapConnection<K, V>(decorated, workerThreads);
+    }
+
+    public static final <K, V> Store<K, V> forceBatchGets(final int maxDelayInMs, final Store<K, V> decorated) {
+        return new MultiGetMap<K, V>(maxDelayInMs, decorated);
+    }
 
 }
