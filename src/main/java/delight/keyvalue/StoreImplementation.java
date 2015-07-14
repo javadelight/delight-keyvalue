2a737a172c79a96c0cf279d8d774986906052e68
mxro <mxro@nowhere.com>
2015-07-14 10:51:10 +1000

    Implementing new database engine Autocommit Tue Jul 14 10:51:10 EST 2015

--- src/main/java/delight/keyvalue/StoreImplementation.java
+++ src/main/java/delight/keyvalue/StoreImplementation.java
@@ -4,6 +4,8 @@ import delight.async.callbacks.SimpleCallback;
 import delight.async.callbacks.ValueCallback;
 import delight.functional.Closure;
 
+import java.util.List;
+
 public interface StoreImplementation<K, V> extends Store<K, V> {
 
     public void clearCache();
@@ -13,5 +15,7 @@ public interface StoreImplementation<K, V> extends Store<K, V> {
     public void getAll(String keyStartsWith, final Closure<StoreEntry<K, V>> onEntry, final SimpleCallback onCompleted);
 
     public void count(String keyStartsWith, final ValueCallback<Integer> callback);
+
+    public void get(List<K> keys, ValueCallback<List<V>> callback);
 
 }
---
+++ src/main/java/delight/keyvalue/jre/internal/MultiGetMap.java
@@ -0,0 +1,5 @@
+package delight.keyvalue.jre.internal;
+
+public class MultiGetMap {
+
+}
