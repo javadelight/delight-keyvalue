e08e274fee89ff2570d41f84292400aed5f2bd38
mxro <mxro@nowhere.com>
2015-08-26 08:55:08 +1000

    Updating Databases API Autocommit Wed Aug 26 08:55:08 EST 2015

--- src/main/java/delight/keyvalue/internal/HashMapAsyncMap.java
+++ src/main/java/delight/keyvalue/internal/HashMapAsyncMap.java
@@ -120,7 +120,7 @@ public class HashMapAsyncMap<K, V> implements StoreImplementation<K, V> {
     @Override
     public void count(final String keyStartsWith, final ValueCallback<Integer> callback) {
         final Value<Integer> count = new Value<Integer>(0);
-        getAll(keyStartsWith, new Closure<StoreEntry<K, V>>() {
+        getAll(keyStartsWith, 0, -1, new Closure<StoreEntry<K, V>>() {
 
             @Override
             public void apply(final StoreEntry<K, V> o) {
