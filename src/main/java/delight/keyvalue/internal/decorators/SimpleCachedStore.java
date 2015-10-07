d043ad1d483f43df44f50cfa5934104acf43b692
mxro <mxro@nowhere.com>
2015-10-08 10:04:52 +1100

    Fixing delete database Autocommit Thu Oct 08 10:04:52 EST 2015

--- src/main/java/delight/keyvalue/internal/decorators/SimpleCachedStore.java
+++ src/main/java/delight/keyvalue/internal/decorators/SimpleCachedStore.java
@@ -22,20 +22,24 @@ class SimpleCachedStore<K, V> implements Store<K, V> {
 
     @Override
     public void put(final K key, final V value, final SimpleCallback callback) {
-        if (value != null) {
-            this.cache.put(key, value);
-        } else {
-            this.cache.put(key, NULL);
+        synchronized (cache) {
+            if (value != null) {
+                this.cache.put(key, value);
+            } else {
+                this.cache.put(key, NULL);
+            }
         }
         decorated.put(key, value, callback);
     }
 
     @Override
     public void putSync(final K key, final V value) {
-        if (value != null) {
-            this.cache.put(key, value);
-        } else {
-            this.cache.put(key, NULL);
+        synchronized (cache) {
+            if (value != null) {
+                this.cache.put(key, value);
+            } else {
+                this.cache.put(key, NULL);
+            }
         }
 
         decorated.putSync(key, value);
@@ -44,7 +48,10 @@ class SimpleCachedStore<K, V> implements Store<K, V> {
     @SuppressWarnings("unchecked")
     @Override
     public void get(final K key, final ValueCallback<V> callback) {
-        final Object fromCache = this.cache.get(key);
+        final Object fromCache;
+        synchronized (cache) {
+            fromCache = this.cache.get(key);
+        }
         if (fromCache != null) {
             if (fromCache == NULL) {
                 callback.onSuccess(null);
@@ -62,7 +69,10 @@ class SimpleCachedStore<K, V> implements Store<K, V> {
     @SuppressWarnings("unchecked")
     @Override
     public V getSync(final K key) {
-        final Object fromCache = this.cache.get(key);
+        final Object fromCache;
+        synchronized (cache) {
+            fromCache = this.cache.get(key);
+        }
         if (fromCache != null) {
             if (fromCache == NULL) {
                 return null;
@@ -77,14 +87,17 @@ class SimpleCachedStore<K, V> implements Store<K, V> {
 
     @Override
     public void remove(final K key, final SimpleCallback callback) {
-        this.cache.remove(key);
+        synchronized (cache) {
+            this.cache.remove(key);
+        }
         this.decorated.remove(key, callback);
     }
 
     @Override
     public void removeSync(final K key) {
-
-        this.cache.remove(key);
+        synchronized (cache) {
+            this.cache.remove(key);
+        }
         this.decorated.removeSync(key);
     }
 
