71ff80a66d498360e5a25fdacc30a25a949c9506
mxro <mxro@nowhere.com>
2015-10-08 10:27:52 +1100

    Fixing delete database Autocommit Thu Oct 08 10:27:52 EST 2015

--- src/main/java/delight/keyvalue/jre/StoresJre.java
+++ src/main/java/delight/keyvalue/jre/StoresJre.java
@@ -5,12 +5,13 @@ import delight.keyvalue.Stores;
 import delight.keyvalue.jre.internal.MultiGetMap;
 import delight.keyvalue.jre.internal.SplitWorkerThreadsMapConnection;
 
+import java.util.Collections;
 import java.util.WeakHashMap;
 
 public class StoresJre {
 
     public static final <K, V> Store<K, V> cacheWithWeakReferences(final Store<K, V> decorated) {
-        return Stores.cache(new WeakHashMap<K, Object>(), decorated);
+        return Stores.cache(Collections.synchronizedMap(new WeakHashMap<K, Object>()), decorated);
     }
 
     public static final <K, V> Store<K, V> divideWork(final int workerThreads, final Store<K, V> decorated) {
