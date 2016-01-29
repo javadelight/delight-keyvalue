91eeed9d6a49b2ce4090c36c05278a0896778ca9
mxro <mxro@nowhere.com>
2016-01-29 14:07:11 +1100

    Developing ObjectHub and improving engine Autocommit Fri Jan 29 14:07:11 EST 2016

--- src/main/java/delight/keyvalue/jre/internal/MultiGetMap.java
+++ src/main/java/delight/keyvalue/jre/internal/MultiGetMap.java
@@ -53,9 +53,8 @@ public final class MultiGetMap<K, V> implements Store<K, V> {
 
         if (!scheduled.isEmpty() || processing.get() > 0) {
             waitTillEmpty();
-            // throw new RuntimeException(
-            // "Multi get map could not be shut down correctly. Items still
-            // processing: " + processing.get());
+            throw new RuntimeException("Multi get map could not be shut down correctly. Items still processing: "
+                    + processing.get() + " Scheduled: " + scheduled.size());
         }
 
     }
