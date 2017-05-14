d68cbb728a1d402c22b67b992cb313a0ce989975
mxro <mxro@nowhere.com>
2017-05-14 10:21:26 +1000

    Recompiling Java from XTend source files Autocommit Sun May 14 10:21:26 AEST 2017

--- src/test/java/delight/keyvalue/tests/TestStringCaseFilters.java
+++ src/test/java/delight/keyvalue/tests/TestStringCaseFilters.java
@@ -13,9 +13,11 @@ public class TestStringCaseFilters {
     final DecodeCaseInsensitiveKey filterOut = new DecodeCaseInsensitiveKey();
     final String lowerCase = filterIn.apply("Something_with_Uppercase");
     Assert.assertEquals("^Something_with_^Uppercase", lowerCase);
-    Assert.assertEquals("Something_with_Uppercase", filterOut.apply(lowerCase));
+    String _apply = filterOut.apply(lowerCase);
+    Assert.assertEquals("Something_with_Uppercase", _apply);
     final String key2 = filterIn.apply("this/is/Nothing/but_a_very-common_KEY");
     Assert.assertEquals("this/is/^Nothing/but_a_very-common_^K^E^Y", key2);
-    Assert.assertEquals("this/is/Nothing/but_a_very-common_KEY", filterOut.apply(key2));
+    String _apply_1 = filterOut.apply(key2);
+    Assert.assertEquals("this/is/Nothing/but_a_very-common_KEY", _apply_1);
   }
 }
